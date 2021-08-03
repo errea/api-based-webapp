5463c214149a8be61f6382fce8ac83014990791c
Arthur Borges <arthurborges.dev@gmail.com>
2021-08-03 13:13:50 -0300

    Create the mockup for calling the function which will display the Comment Popup

---
+++ src/comments.js
@@ -0,0 +1,3 @@
+const displayCommentPopup = (id) => {
+  
+}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
 import './style.css';
 import Logo from './img/logo.png';
+import { displayCommentPopup } from './comments.js';
+
+const button = document.querySelectorAll('[data-id="40995"]')[0];
+
+button.addEventListener('click', (e) => {
+  const movieId = e.target.getAttribute('data-id');
+  displayCommentPopup(movieId);
+});
