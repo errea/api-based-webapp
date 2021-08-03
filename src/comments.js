5ab7d5cbd927e0bbdfb164a7011524e879d8a750
Arthur Borges <arthurborges.dev@gmail.com>
2021-08-03 13:49:45 -0300

    Successfully add a dinamic popup consisting of a image above the button

--- src/comments.js
+++ src/comments.js
@@ -3,7 +3,18 @@ import { getMovieById } from './moviesApi.js';
 const displayCommentPopup = async (id) => {
   const movie = await getMovieById(id);
 
-  console.log(movie);
+  generatePopupContent(movie);
+}
+
+const generatePopupContent = (movie) => {
+  const popup = document.getElementById('popup');
+  
+  popup.innerHTML = "";
+  popup.insertAdjacentHTML('beforeend', ` 
+    <div>
+      <img src="${movie.image.medium}" />
+    <div>`
+  );
 }
 
 export { displayCommentPopup }
