99f11a2d78944f22e0d35435fff934487cbbea14
Arthur Borges <arthurborges.dev@gmail.com>
2021-08-03 14:15:30 -0300

    Add popup with image and description, but without icons and close button

--- src/comments.js
+++ src/comments.js
@@ -11,9 +11,23 @@ const generatePopupContent = (movie) => {
   
   popup.innerHTML = "";
   popup.insertAdjacentHTML('beforeend', ` 
-    <div>
-      <img src="${movie.image.medium}" />
-    <div>`
+    <div class="popup-container">
+      <div class="inner-content>
+        <div class="photo-close">
+          <img src="${movie.image.medium}" />
+          <input type="button" class="close-popup"><i class="fas fa-times"></i>
+        </div>
+        <h3>${movie.name}</h3>
+        <div class="movie-description-1">
+          <p>Type: ${movie.type}</p>
+          <p>Language: ${movie.language}</p>
+        </div>
+        <div class="movie-description-2">
+          <p>Status: ${movie.status}</p>
+          <p>Premiered: ${movie.premiered}</p>
+        </div>
+      <div>
+    </div>`
   );
 }
 
